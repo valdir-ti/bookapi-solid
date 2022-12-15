be234f4bf1b04845e2ac5b13d64699d87de80e94
Valdir Silva <valdir.ti@gmail.com>
2022-12-15 00:34:15 -0300

    feat: create user repositoy in memory and tests

--- src/useCases/Users/LoginUser/LoginUserUseCase.spec.ts
+++ src/useCases/Users/LoginUser/LoginUserUseCase.spec.ts
@@ -1,5 +1,46 @@
-describe("Should sum", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    expect(1 + 1).toBe(2)
+import { IUserRepositoryInMemory } from "../../../repositories/implementations/in-memory/IUserRepositoryInMemory"
+import { LoginUserUseCase } from "./LoginUserUseCase"
+
+let userRepositoryInMemory: IUserRepositoryInMemory
+let loginUserUseCase: LoginUserUseCase
+
+describe("Login User Use Case", () => {
+  beforeEach(() => {
+    userRepositoryInMemory = new IUserRepositoryInMemory()
+    loginUserUseCase = new LoginUserUseCase(userRepositoryInMemory)
+  })
+
+  test("should returns undefined if not found an user", async () => {
+    const login = await userRepositoryInMemory.findOne("any_name")
+    expect(login).toBe(undefined)
+  })
+
+  test("should returns an user if correct username is send", async () => {
+    const login = await userRepositoryInMemory.findOne("any_username")
+    expect(login.username).toEqual("any_username")
+    expect(login.email).toEqual("any_email")
+  })
+
+  test("should throw an error if username or password is not sent", async () => {
+    const data = { username: "", password: "" }
+
+    expect(async () => {
+      await loginUserUseCase.execute(data)
+    }).rejects.toBeInstanceOf(Error)
+  })
+
+  test("should throw an error if username or password are incorrect", async () => {
+    const data = { username: "", password: "" }
+    expect(async () => {
+      await loginUserUseCase.execute(data)
+    }).rejects.toBeInstanceOf(Error)
+  })
+
+  test("should throw an error if password is incorrect", async () => {
+    const data = { username: "any_username", password: "any" }
+
+    expect(async () => {
+      await loginUserUseCase.execute(data)
+    }).rejects.toBeInstanceOf(Error)
   })
 })
